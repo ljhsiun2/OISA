03ddb850900337e7132b403b935472b91d7cd546
ljhsiun2 <ljhsiun2@illinois.edu>
2018-03-18 09:40:40 -0700

    kinda DNN I think? and cleanup

--- benchmarks/FC.c
+++ benchmarks/FC.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../multi2sim/bin/benchmarks/primitives/path_oram/oram.h"
+//#include "../multi2sim/bin/benchmarks/primitives/path_oram/oram.h"
 
-void matrix_mul(int* m1, int* m2, int* m3, int m1_size, int m2_size){
-	for(int i=0; i<m1_size; i++)
+#define MAT_SIZE 10
+
+int matrix_mul(int* m1, int* m2){
+	
+	int retVal = 0;
+	for(int i=0; i<MAT_SIZE; i++)
 	{
-		for(int j=0; j<m2_size; j++)
-		{
-			
-		}
+		retVal += m1[i]*m2[i];
 	}
+	return (retVal < 0) ? 0 : retVal;
 }
 
 int main(){
+	int arr1[MAT_SIZE] = {2, 1, 5, 7, 8, 2, 3, 4, 9};
+	int arr2[MAT_SIZE][MAT_SIZE] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+									1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+									2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+									3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+									4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+									5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+									6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+									7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+									8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+									9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
+	int arr3[MAT_SIZE];
+	for(int i =0; i<MAT_SIZE; i++)
+	{
+		arr3[i] = matrix_mul(arr1, arr2[i]);
+		printf("Value in element %d: %d\n", i, arr3[i]);
+	}
+	
+
 
 	return 0;
 }
